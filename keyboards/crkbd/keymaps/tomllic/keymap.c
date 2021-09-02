2d4f23a296357cb4cd32ef343d27121f2dc41c04
unknown <tomllic@gmail.com>
2021-09-02 23:15:34 +0300

    hoped that this will fix the OLED

--- keyboards/crkbd/keymaps/tomllic/keymap.c
+++ keyboards/crkbd/keymaps/tomllic/keymap.c
@@ -47,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINUS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-SFT_T(KC_BSPC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      KC_BSPC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LCTRL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+     LCTL_T(KC_BSLS),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                       GUI_T(KC_DEL), MO(LOWER), KC_SPC,     KC_ENT,   MO(RAISE), KC_LALT
+                                       GUI_T(KC_DEL), MO(LOWER), KC_SPC,     SFT_T(KC_ENT),   MO(RAISE), KC_LALT
                                       //`--------------------------'  `--------------------------'
 
   ),
